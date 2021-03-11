b3c84ebe87ced069c41468fa6733d9d122463795
ProgressiveFoo <bboypamik@gmail.com>
2021-03-12 00:22:22 +0100

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <transition name="toast">
+    <Toast v-if="showToast" />
+    </transition>
+    <Todos @badValue="triggerToast" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast }
+  }
+}
+</script>
+
+<style>
+.toast-enter-from{
+  opacity: 0;
+  transform:  translateY(-60px);
+}
+.toast-enter-active{
+  transition: all 0.3s ease;
+}
+.toast-leave-to{
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-leave-active{
+  transition: all 0.3s ease;
+}
+</style>
